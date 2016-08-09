d957a32a9abe14475655ace527a6d9b2a04956f3
Daniel Costa <danielcosta@gmail.com>
2016-08-09 01:56:42 -0300

    Updating to Phalcon 3.0.x on php:fpm-alpine

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
+cd /tmp \
+&& curl -o phalcon.zip https://github.com/phalcon/cphalcon/archive/3.0.0.tar.gz
 apk add --no-cache git bash re2c build-base autoconf pcre-dev \
 && cd /usr/local/lib/ \
-&& git clone --depth=1 https://github.com/phalcon/zephir.git \
-&& cd /usr/local/lib/zephir \
-&& ./install-nosudo -c \
-&& git clone --branch 2.1.x --depth=1 https://github.com/phalcon/cphalcon.git /usr/local/lib/cphalcon \
-&& cd /usr/local/lib/cphalcon \
-&& /usr/local/lib/zephir/bin/zephir fullclean \
-&& /usr/local/lib/zephir/bin/zephir build —backend=ZendEngine3 \
+&& git clone --branch 3.0.x --depth=1 https://github.com/phalcon/cphalcon.git /usr/local/lib/cphalcon \
+&& cd /usr/local/lib/cphalcon/build \
+&& ./install \
 && echo "extension=phalcon.so" >> /usr/local/etc/php/conf.d/40-phalcon.ini \
-&& cp /usr/local/lib/cphalcon/ext/modules/phalcon.so /data
+&& cp /usr/local/lib/php/extensions/no-debug-non-zts-20151012/phalcon.so /data
