5df2d07756c360daeff48237c6de8df7cefc50e5
Daniel Costa <danielcosta@gmail.com>
2016-08-09 02:05:29 -0300

    Fixing build

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd /tmp \
-&& curl -o phalcon.zip https://github.com/phalcon/cphalcon/archive/3.0.0.tar.gz
-apk add --no-cache git bash re2c build-base autoconf pcre-dev \
+&& apk add --no-cache git bash re2c build-base autoconf pcre-dev \
 && cd /usr/local/lib/ \
 && git clone --branch 3.0.x --depth=1 https://github.com/phalcon/cphalcon.git /usr/local/lib/cphalcon \
 && cd /usr/local/lib/cphalcon/build \
